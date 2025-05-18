505d52bcdfb45e68193443397859490674d10a8d
gauravtest25 <gauravsh2505@gmail.com>
2025-05-18 13:03:24 +0530

    adding secondLargest program

---
+++ SecondLargest.java
@@ -0,0 +1,23 @@
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr = {10, 45, 2, 99, 23, 77};
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("No second largest element found.");
+        } else {
+            System.out.println("Second Largest Element: " + secondLargest);
+        }
+    }
+}
